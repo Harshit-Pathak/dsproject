fb7d2a98a56fbf6541744a0480ebb57f5ae46caf
Harshit Pathak <harshitpathak29@gmail.com>
2020-09-01 22:45:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+        ),
+        home: Scaffold(
+          backgroundColor: Colors.deepPurpleAccent,
+          appBar: AppBar(centerTitle:true,
+            backgroundColor: Colors.white12,
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.white70,width: 3.0),
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(10)
+                )
+            ),
+            title: Text('Data Structure and Algorithms'),
+          ),
+        )
+    );
+  }
+}
